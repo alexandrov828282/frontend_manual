76ff6524c1382c21d2a76a1fd632cc204ba7b6b5
alexandrov828282 <alexandrov828282@gmail.com>
2020-10-18 19:10:34 +0500

    202010181910

--- js/tags.js
+++ js/tags.js
@@ -5,28 +5,3 @@ tags[0] = {
     'tagValue':'строчные элементы'
 }
 
-tags[1] = {
-    'id': 2,
-    'tagValue':'изображения'
-}
-
-tags[0] = {
-    'id': 3,
-    'tagValue':'картинки'
-}
-
-tags[0] = {
-    'id': 4,
-    'tagValue':'медиа'
-}
-
-tags[0] = {
-    'id': 5,
-    'tagValue':'графика'
-}
-
-tags[0] = {
-    'id': 6,
-    'tagValue':'рисунок'
-}
-
