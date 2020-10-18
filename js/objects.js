70e446ffad7d0553eeed6215ac457133b81e952e
alexandrov828282 <alexandrov828282@gmail.com>
2020-10-18 18:04:26 +0500

    181020201804

---
+++ js/objects.js
@@ -0,0 +1,71 @@
+let arrObjects = [];
+
+arrObjects[0] = {
+    'id'   : 1,
+    'title': 'Тег <img>', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
+
+arrObjects[1] = {
+    'id'   : ,
+    'title': '', 
+    'tags' : [],
+    'link' : ''
+};
---
+++ js/tags.js
@@ -0,0 +1,32 @@
+let tags = [];
+
+tags[0] = {
+    'id': 1,
+    'tagValue':'строчные элементы'
+}
+
+tags[1] = {
+    'id': 2,
+    'tagValue':'изображения'
+}
+
+tags[0] = {
+    'id': 3,
+    'tagValue':'картинки'
+}
+
+tags[0] = {
+    'id': 4,
+    'tagValue':'медиа'
+}
+
+tags[0] = {
+    'id': 5,
+    'tagValue':'графика'
+}
+
+tags[0] = {
+    'id': 6,
+    'tagValue':'рисунок'
+}
+
