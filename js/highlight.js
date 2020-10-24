f369872baa8fe8b81240d30ab4ed31faa3be6476
alexandrov828282 <alexandrov828282@gmail.com>
2020-10-24 20:16:58 +0500

    202010242016

--- js/highlight.js
+++ js/highlight.js
@@ -31,7 +31,6 @@
 // // забираем все элементы с классом highlight
 // let elems = document.getElementsByClassName("highlight");
 
-
 // for (const elem of elems) {  //берем один элемент из пачки тегов с классом highlight
 
 //     tempstr = elem.innerHTML; //содержимое тега
@@ -44,13 +43,9 @@
 
 //         if (indexMarkerBegin != 0) {
 
-
-        
 //         }
 //     }
 // }
-
-
 
 // for (const elem of elems) {
 //     let resStr = '';
@@ -69,8 +64,6 @@
 //         elems[i] = separator[j];
 //     }
 // }
-
-
 
 // let method = '.concat('
 
@@ -103,19 +96,17 @@
 // let finalStr = '';
 // let selectStr = '';
 
-
 // jsMethodElems = ['.method1(','.method2(','.method3('];
-
 
 // //подучим коллекцию тегов с классом highlight
 // let htmlTagElems = document.getElementsByClassName("highlight");
 
 // //берем каждый тег из коллекции
 // for (const htmlTagElem of htmlTagElems) {
-    
+
 //     //берем каждый метод из списка методов
 //     for (const jsMethodElem of jsMethodElems) {
-        
+
 //         //находим этот иетод в html тексте, определяем индекс начала
 //         indexMarkBegin = htmlTagElem.indexOf(jsMethodElem, indexOrdinaryBegin);
 
@@ -127,15 +118,11 @@
 
 //         }
 
-        
-
 //         //
 
 //     }
 
-
 // }
-
 
 let separatorSymbols = [
     "~",
@@ -160,80 +147,76 @@ let separatorSymbols = [
     "=",
     "\\",
     "|",
-    ' ',
-    '.'
+    " ",
+    ".",
 ];
 
-let currentWord = '';
+let currentWord = "";
 let newHtmlString = [];
 // let coincide = false;
 
-let jsMethodElems = ['method1','method2','method3'];
+let jsMethodElems = ["method1", "method2", "method3"];
 
 let htmlTagElems = document.getElementsByClassName("highlight");
 
-coincide = str => {
+//встретился ли разделитель
+separatorOn = (str) => {
     for (let separatorSymbol of separatorSymbols) {
-        if (str == separatorSymbol) return true; 
+        if (str == separatorSymbol) return true;
+    }
+    return false;
+};
+
+//является ли слово методом
+methodOn = (str) => {
+    for (let jsMethodElem of jsMethodElems) {
+        if (str == jsMethodElem) return true;
     }
     return false;
 }
 
 for (let htmlTagElem of htmlTagElems) {
-    let htmlSymbols = htmlTagElem.innerHTML.split('');
-    
-    for (let htmlSymbol of htmlSymbols) {
-        
-        // coincide = false;
-        // for (let separatorSymbol of separatorSymbols) {
-        //     if (htmlSymbol == separatorSymbol) coincide = true;
-        // }
+    let htmlSymbols = htmlTagElem.innerHTML.split("");
 
-        if (coincide(htmlSymbol)){
-            for (let jsMethodElem of jsMethodElems) {
-                if (currentWord == jsMethodElem){
-                    w(`совпадение ${currentWord} == ${jsMethodElem}`);
-                }
-                else{
-                    w(`НЕ совпадение ${currentWord} != ${jsMethodElem}`);
-                }
+    for (let htmlSymbol of htmlSymbols) {
+        if (separatorOn(htmlSymbol)) {
+            w(currentWord);
+            if (methodOn(currentWord)) {
+                //действия при совпадении
+            }
+            else{
+                //действия при не совпадении
             }
             currentWord = '';
         }
         else{
             currentWord += htmlSymbol;
         }
-
     }
-
 }
 
-
-
-
 // currentWord += htmlSymbol;
-        //     }
-        //     else{
-        //         console.log(currentWord);
-        //         currentWord = '';
-    //             for (let jsMethodElem of jsMethodElems) {
-    //                 if (currentWord == jsMethodElem){
-    //                     newHtmlString.push(` <span class="method">${currentWord}</span>`);
-    //                 }
-    //                 else{
-    //                     newHtmlString.push(currentWord);
-    //                     currentWord = '';
-    //                 }
-    //             }
-        //     }
-        // }
-    // }
-    
-    // htmlTagElem.innerHTML = newHtmlString.join('');
-    // console.log(htmlSymbols);
+//     }
+//     else{
+//         console.log(currentWord);
+//         currentWord = '';
+//             for (let jsMethodElem of jsMethodElems) {
+//                 if (currentWord == jsMethodElem){
+//                     newHtmlString.push(` <span class="method">${currentWord}</span>`);
+//                 }
+//                 else{
+//                     newHtmlString.push(currentWord);
+//                     currentWord = '';
+//                 }
+//             }
+//     }
+// }
 // }
 
-function w(str){
-    console.log(str)
-} 
+// htmlTagElem.innerHTML = newHtmlString.join('');
+// console.log(htmlSymbols);
+// }
 
+function w(str) {
+    console.log(str);
+}
