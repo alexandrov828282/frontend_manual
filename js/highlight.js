61ad7e6eec135d1624cae00c9ea00e70b7e26ab7
alexandrov828282 <alexandrov828282@gmail.com>
2020-10-25 14:33:55 +0500

    202010251433

--- js/highlight.js
+++ js/highlight.js
@@ -1,4 +1,8 @@
 let separatorSymbols = [
+    "&lt;",
+    "&gt;",
+    "//",
+    "\\",
     "~",
     "`",
     "!",
@@ -19,90 +23,93 @@ let separatorSymbols = [
     "*",
     "/",
     "=",
-    "\\",
     "|",
     " ",
     ".",
-    '<',
-    '>',
-    '//'
+    "<",
+    ">"
 ];
 
 let currentWord = "";
 let newHtmlString = [];
-// let coincide = false;
-
-// let jsMethodElems = ["method1", "method2", "method3"];
 
 let allHighlightElems = {
-    'js':{
-        'Method':[
-            "concat"
-        ],
-        'Object':[
-            'object1', 'object2','object3'
-        ],
-        'Property':[
-            'property1', 'property2', 'property3'
-        ]
+    js: {
+        Method: ["concat"],
+        Object: ["object1", "object2", "object3"],
+        Property: ["property1", "property2", "property3"],
     },
 
-    'html':{
-        'Tag':[
-            '<br>','div','h1'
-        ]
-    }
-}
-
-
-
-let htmlTagElems = document.getElementsByClassName("highlight");
-
-//встретился ли разделитель
-separatorOn = (str) => {
-    for (let separatorSymbol of separatorSymbols) {
-        if (str == separatorSymbol) return true;
-    }
-    return false;
+    html: {
+        Tag: ["br", "div", "h1"],
+    },
 };
 
+let htmlTagElems = document.getElementsByClassName("highlight");
+let htmlText = '';
+
 for (let htmlTagElem of htmlTagElems) {
-    let htmlSymbols = htmlTagElem.innerHTML.split("");
 
-    for (let htmlSymbol of htmlSymbols) {
-        if (separatorOn(htmlSymbol)) {
+    htmlText = htmlTagElem.innerHTML;
+    let numSymbol = 0;
+    while(numSymbol < htmlText.length){
+
+        for (let separatorSymbol of separatorSymbols) {
             
-            for (let elemTechnology in allHighlightElems) {
-               
-                for (let elemSection in allHighlightElems[elemTechnology]) {
-
-                    for (let elem of allHighlightElems[elemTechnology][elemSection]) {
-                        
-                        if (currentWord == elem){
-                            currentWord = `<span class="${elemTechnology}${elemSection}">${currentWord}</span>`;
-                            w(currentWord);
-
-                        }
-
-                    }
-                }
+            if (numSymbol+separatorSymbol.length > htmlText.length) continue;
+            
+            if(htmlText.substr(numSymbol, separatorSymbol.length) == separatorSymbol){
+                w(separatorSymbol);
+                numSymbol += separatorSymbol.length;
+                break;
             }
-
-            currentWord += htmlSymbol;
-            newHtmlString.push(currentWord);
-            currentWord = '';
         }
-        else{
-            currentWord += htmlSymbol;
-        }
+        
+        numSymbol++;
     }
-    
-    htmlTagElem.innerHTML = newHtmlString.join('');
-    newHtmlString.length = 0;
-    // w(htmlTagElem.innerHTML);
+
 }
 
 
+//встретился ли разделитель
+// separatorOn = (str) => {
+//     for (let separatorSymbol of separatorSymbols) {
+//         if (str == separatorSymbol) return true;
+//     }
+//     return false;
+// };
+
+// for (let htmlTagElem of htmlTagElems) {
+//     let htmlSymbols = htmlTagElem.innerHTML.split("");
+//     w(htmlSymbols);
+//     for (let htmlSymbol of htmlSymbols) {
+//         if (separatorOn(htmlSymbol)) {
+
+//             for (let elemTechnology in allHighlightElems) {
+
+//                 for (let elemSection in allHighlightElems[elemTechnology]) {
+
+//                     for (let elem of allHighlightElems[elemTechnology][elemSection]) {
+
+//                         if (currentWord == elem){
+//                             currentWord = `<span class="${elemTechnology}${elemSection}">${currentWord}</span>`;
+//                         }
+//                     }
+//                 }
+//             }
+
+//             currentWord += htmlSymbol;
+//             newHtmlString.push(currentWord);
+//             currentWord = '';
+//         }
+//         else{
+//             currentWord += htmlSymbol;
+//         }
+//     }
+
+//     htmlTagElem.innerHTML = newHtmlString.join('');
+//     newHtmlString.length = 0;
+// }
 
 function w(str) {
     console.log(str);
